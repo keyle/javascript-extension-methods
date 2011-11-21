84f3c0e69dfe0d0a84a30bc441846fd2c5c48798
keyle <keyle@capsule.org>
2011-11-21 20:35:50 +1000

    added some methods from http://stackoverflow.com/questions/498970/how-to-trim-a-string-in-javascript

--- utils/string.extension.methods.js
+++ utils/string.extension.methods.js
@@ -41,3 +41,17 @@ String.prototype.f = String.prototype.format;
 String.prototype.stripTags = function() {
 	return this.replace(/(<([^>]+)>)/ig, '');
 }
+
+
+String.prototype.trim=function()
+	{return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');};
+
+String.prototype.ltrim=function()
+	{return this.replace(/^\s+/,'');}
+
+String.prototype.rtrim=function()
+	{return this.replace(/\s+$/,'');}
+
+String.prototype.fulltrim=function()
+	{return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');}
+
